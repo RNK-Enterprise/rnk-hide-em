4bd8ee2c3c5f992b4be51448ecffa10c6226c04b
The Curator <asgardinnovationsnc@gmail.com>
2026-02-05 17:28:10 -0500

    feat: clean production release - remove all protection/encryption infrastructure, fix module manifest, restore functionality

--- scripts/module.js
+++ scripts/module.js
@@ -9,4 +9,30 @@ import { log } from "./utils.js";
 import { registerSettings } from "./settings.js";
 import { initHook, readyHook, renderMacroDirectoryHook, hotbarCollapseHook } from "./hooks.js";
 
-let _c=null;function _h(a,b){return a*b}function _s(a,b){return a-b}function _m(a,b){return a*b}function _r(){Handlebars.registerHelper('math',_h);Handlebars.registerHelper('subtract',_s);Handlebars.registerHelper('multiply',_m)}Hooks.once("init",async()=>{log("Initializing RNK Hide 'Em");_c=window.RNKCryptoCore;await _c.init();if(!_c.validate(_c._chk())){console.warn("RNK Hide 'Em | Core integrity check failed");return}log("RNK Hide 'Em | Core check passed");registerSettings();_r();initHook()});Hooks.once("ready",()=>{if(!_c){console.warn("RNK Hide 'Em | Core not initialized");return}log("RNK Hide 'Em ready");readyHook()});Hooks.on("renderMacroDirectory",renderMacroDirectoryHook);Hooks.on("collapseHotbar",hotbarCollapseHook);Hooks.on("expandHotbar",hotbarCollapseHook);log("Module loaded");
+// Register Handlebars helpers
+function registerHelpers() {
+  Handlebars.registerHelper('math', (a, b) => a * b);
+  Handlebars.registerHelper('subtract', (a, b) => a - b);
+  Handlebars.registerHelper('multiply', (a, b) => a * b);
+}
+
+// Initialize on game init
+Hooks.once("init", async () => {
+  log("Initializing RNK Hide 'Em");
+  registerSettings();
+  registerHelpers();
+  initHook();
+});
+
+// Ready hook
+Hooks.once("ready", () => {
+  log("RNK Hide 'Em ready");
+  readyHook();
+});
+
+// Register event listeners
+Hooks.on("renderMacroDirectory", renderMacroDirectoryHook);
+Hooks.on("collapseHotbar", hotbarCollapseHook);
+Hooks.on("expandHotbar", hotbarCollapseHook);
+
+log("Module loaded");
